aa87de4c6275780e94ad2a76ca9f971bade57b15
kin393 <34294833+kin393@users.noreply.github.com>
2023-01-22 12:48:00 -0500

    Create currying.js

---
+++ currying/currying.js
@@ -0,0 +1,14 @@
+function curry(f) { //curry(f) does the currying transform 
+return function(a) { 
+ return function(b) { 
+   return f(a, b);
+ };
+};
+};
+
+function sum(a, b) { return a + b; }
+
+let curriedSum = curry(sum);
+
+alert( curriedSum(1)(2) ); 
+
